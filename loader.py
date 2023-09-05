d5fc6d9ac001c2c35c473a836f294dee43234359
ChrismaInvestor <snake1950@163.com>
2023-09-05 08:41:19 +0800

    Updated

--- loader.py
+++ loader.py
@@ -1,15 +1,30 @@
 import akshare as ak
-stock_zh_a_minute_df = ak.stock_zh_a_minute(symbol='sz000876', period='1', adjust="qfq")
-
 import requests
-host="http://localhost:8080"
+host = "http://localhost:8080"
 url = host + "/price"
-body={"code":"sh113658","price":115,"volume":7,"time":"2023-08-17 10:43:00"}
-requests.post(url=rul, json=body)
 
-stockList = ak.stock_info_a_code_name()
+stock_list_df = ak.stock_info_a_code_name()
 
-for index, row in df.iterrows():
-    print(index, row['name'], row['age'])
-
-
+for index, row in stock_list_df.iterrows():
+    # if (row['code'].startswith("0") | row['code'].startswith("3")):
+    #     stock_zh_a_minute_df = ak.stock_zh_a_minute(
+    #         symbol="sz" + row['code'], period='1', adjust="qfq")
+    #     for index2, row2 in stock_zh_a_minute_df.iterrows():
+    #         body = {"code": row['code'], "price": row2['close'],
+    #                 "volume": row2['volume'], "time": row2['day']}
+            
+    #         try:
+    #             r = requests.post(url=url, json=body)
+    #         except:
+    #             print("Something went wrong")
+    #         finally:
+    #             r.close()
+    if (row['code'].startswith("6")):
+        stock_zh_a_minute_df = ak.stock_zh_a_minute(
+            symbol="sh" + row['code'], period='1', adjust="qfq")
+        for index2, row2 in stock_zh_a_minute_df.iterrows():
+            body = {"code": row['code'], "price": row2['close'],
+                    "volume": row2['volume'], "time": row2['day']}
+            r = requests.post(url=url, json=body)
+            r.close()
+# requests.post(url=rul, json=body)
