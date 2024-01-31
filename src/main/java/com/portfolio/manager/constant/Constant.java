4d3b311003400cae2958108575f4d03da6c67c3d
Chris Ma <snake1950@163.com>
2024-01-31 18:49:06 +0800

    cleanup

--- src/main/java/com/portfolio/manager/constant/Constant.java
+++ src/main/java/com/portfolio/manager/constant/Constant.java
@@ -8,10 +8,6 @@ public class Constant {
 
     public static final long CONVERTIBLE_BOND_MULTIPLE = 10L;
 
-//    public static final String CROWN_TAKE_PROFIT = "1.075";
-//
-//    public static final String CROWN_STOP_LOSS = "0.975";
-
     public static final BigDecimal CROWN_TAKE_PROFIT = new BigDecimal("1.075");
 
     public static final BigDecimal CROWN_STOP_LOSS = new BigDecimal("0.975");
