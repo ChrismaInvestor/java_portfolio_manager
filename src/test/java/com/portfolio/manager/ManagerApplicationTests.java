e82dd39865a8c6b32ecf51f380a2c86f41535e51
Chris Ma <snake1950@163.com>
2024-02-29 19:29:24 +0800

    Update front end files

--- src/test/java/com/portfolio/manager/ManagerApplicationTests.java
+++ src/test/java/com/portfolio/manager/ManagerApplicationTests.java
@@ -1,45 +1,35 @@
 package com.portfolio.manager;
 
+import com.portfolio.manager.domain.Portfolio;
+import com.portfolio.manager.domain.Position;
+import com.portfolio.manager.dto.BidAskBrokerDTO;
+import com.portfolio.manager.service.PortfolioService;
+import com.portfolio.manager.task.TradeTaskService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 
+@Slf4j
 @SpringBootTest
 class ManagerApplicationTests {
+    @Resource
+    PortfolioService portfolioService;
 
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> ans = new ArrayList<>();
-        Deque<Integer> window = new LinkedList<>();
-        window.offer(nums[0]);
-        for (int i = 1; i < k; i++) {
-            if (nums[i] > window.peekFirst()) {
-                window.poll();
-            }
-            window.offer(nums[i]);
-        }
-        ans.add(window.peekFirst());
-        for (int i = k; i < nums.length; i++) {
-            if(window.size() == k){
-                window.poll();
-            }
-            if (window.peekFirst() != null && nums[i] > window.peekFirst()) {
-                while (window.peekFirst() != null && nums[i] > window.peekFirst()) {
-                    window.poll();
-                }
-            }
-            window.offer(nums[i]);
-            ans.add(window.peekFirst());
-        }
-        return ans.stream().mapToInt(i -> i).toArray();
-    }
+    @Resource
+    TradeTaskService tradeTaskService;
 
     @Test
     void contextLoads() {
-        String str = "rat";
-        char[] chars = str.toCharArray();
-        Arrays.sort(chars);
-        System.out.println(chars);
+        Portfolio portfolio = portfolioService.listPortfolio().get(0);
+        List<Position> positions = portfolioService.listPosition(portfolio.getName());
+        log.info("{}", BigDecimal.valueOf(50L).divide(BigDecimal.valueOf(1330L), 2, RoundingMode.HALF_UP).compareTo(BigDecimal.valueOf(0.2d)) < 0);
+//        BidAskBrokerDTO bidAskBrokerDTO = new BidAskBrokerDTO();
+//        tradeTaskService.autoSellForCrown(bidAskBrokerDTO,portfolio,positions);
     }
 
 }
