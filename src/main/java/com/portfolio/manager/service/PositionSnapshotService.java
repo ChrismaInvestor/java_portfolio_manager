ad87302211a9d7c0b18089f63a6a0339ca0cfb13
Chris Ma <snake1950@163.com>
2024-05-03 22:47:34 +0800

    Supported lock profit and let profit run

--- src/main/java/com/portfolio/manager/service/PositionSnapshotService.java
+++ src/main/java/com/portfolio/manager/service/PositionSnapshotService.java
@@ -2,7 +2,6 @@ package com.portfolio.manager.service;
 
 import com.portfolio.manager.domain.Position;
 import com.portfolio.manager.domain.PositionSnapshot;
-import com.portfolio.manager.dto.OrderDTO;
 
 import java.util.List;
 
