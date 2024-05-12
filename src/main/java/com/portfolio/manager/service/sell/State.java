e8bb49fd395a3e257080bfdff291a566b3a34f93
Chris Ma <snake1950@163.com>
2024-05-12 23:33:16 +0800

    Added the slump detect mechanism

--- src/main/java/com/portfolio/manager/service/sell/State.java
+++ src/main/java/com/portfolio/manager/service/sell/State.java
@@ -2,7 +2,6 @@ package com.portfolio.manager.service.sell;
 
 import com.portfolio.manager.constant.Constant;
 import com.portfolio.manager.dto.BidAskBrokerDTO;
-import com.portfolio.manager.util.Util;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
@@ -21,16 +20,17 @@ public abstract class State {
         if (bid1PricesSlidingWindow.isEmpty()){
             return false;
         }
-        var max = this.bid1PricesSlidingWindow.peekFirst();
-        var min = this.findMin(bid1PricesSlidingWindow);
-        log.info("max: {}, min: {}", max, min);
-        return Util.priceMovementDivide(max.subtract(min).doubleValue(), bidAskBrokerDTO.lastClose()).compareTo(Constant.CROWN_MAX_DRAW_DOWN) <= 0;
+        var max = this.findMax(bid1PricesSlidingWindow);
+//        var min = this.findMin(bid1PricesSlidingWindow);
+        log.info("max: {}", max);
+        return false;
+//        return Util.priceMovementDivide(max.subtract(min).doubleValue(), bidAskBrokerDTO.lastClose()).compareTo(Constant.CROWN_MAX_DRAW_DOWN) <= 0;
     }
 
     void updateBid1PricesSlidingWindow(BidAskBrokerDTO bidAskBrokerDTO){
-        while (!bid1PricesSlidingWindow.isEmpty() && BigDecimal.valueOf(bidAskBrokerDTO.bidPrice1()).compareTo(bid1PricesSlidingWindow.peekLast()) >= 0) {
-            bid1PricesSlidingWindow.pollLast();
-        }
+//        while (!bid1PricesSlidingWindow.isEmpty() && BigDecimal.valueOf(bidAskBrokerDTO.bidPrice1()).compareTo(bid1PricesSlidingWindow.peekLast()) >= 0) {
+//            bid1PricesSlidingWindow.pollLast();
+//        }
         this.bid1PricesSlidingWindow.offer(BigDecimal.valueOf(bidAskBrokerDTO.bidPrice1()));
         while (bid1PricesSlidingWindow.size() > Constant.SLUMP_MAX_SECONDS){
             bid1PricesSlidingWindow.poll();
@@ -41,12 +41,29 @@ public abstract class State {
         return this.getClass().getName();
     }
 
-    private BigDecimal findMin(ConcurrentLinkedDeque<BigDecimal> deque) {
+    private BigDecimal findMax(ConcurrentLinkedDeque<BigDecimal> deque) {
         Iterator<BigDecimal> iterator = deque.descendingIterator();
-        BigDecimal min = deque.peekFirst();
+        BigDecimal max = deque.peekFirst();
         if (deque.size() == 1){
-            return min;
+            return max;
         }
+        while (iterator.hasNext()) {
+            BigDecimal value = iterator.next();
+            if (value.compareTo(max) >= 0) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private BigDecimal findMin(ConcurrentLinkedDeque<BigDecimal> deque, BigDecimal max) {
+        BigDecimal min = max;
+        if (deque.size() == 1){
+            return max;
+        }
+
+        Iterator<BigDecimal> iterator = deque.descendingIterator();
+        boolean start = false;
         while (iterator.hasNext()) {
             BigDecimal value = iterator.next();
             if (value.compareTo(min) < 0) {
